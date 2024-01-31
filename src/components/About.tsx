345f41d3d4da580183c4448ae89efd68e9cf98dd
Muhsin-42 <muhsinofficial21@gmail.com>
2024-01-31 16:16:56 +0530

    Bug fixed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,14 @@
-'use client'
+"use client";
 import React from "react";
 import SectionHeading from "./SectionHeading";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 const About = () => {
   return (
-    <motion.section className=" mb-28 max-w-[45rem] text-center leading-8"
-        initial={{opacity: 0, y:100}}
-        animate={{opacity: 1, y: 0}}
-        transition={{delay: 0.175}}
+    <motion.section
+      className=" mb-28 max-w-[45rem] text-center leading-8"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
     >
       <SectionHeading text="About me" />
       <p className="mb-3">
@@ -39,7 +40,7 @@ const About = () => {
         {`. I'm also
         learning how to play the guitar.`}
       </p>
-    </section>
+    </motion.section>
   );
 };
 

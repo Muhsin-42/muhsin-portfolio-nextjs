4a9e5dbee303905d5aebbbd3f66029f44267f096
Muhsin-42 <muhsinofficial21@gmail.com>
2024-01-31 19:37:26 +0530

    Projects mapped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import About from "@/components/Sections/About";
 import Intro from "@/components/Sections/Intro";
+import Projects from "@/components/Sections/Projects";
 import SectionDivider from "@/components/Shared/SectionDivider";
-
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider />
       <About />
+      <SectionDivider />
+      <Projects />
       <SectionDivider />
     </main>
   );

d26ac217d8dc2e39ca5d7e12efabdeab137f42ad
Muhsin-42 <muhsinofficial21@gmail.com>
2024-02-01 01:32:13 +0530

    Done skills

---
+++ src/components/Sections/Skills.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import SectionHeading from "../Shared/SectionHeading";
+import { SKILLS_DATA } from "@/lib/data";
+import useSectionInView from "@/hook/useSectionInView";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
+
+const Skills = () => {
+  const { ref } = useSectionInView("Skills", 0.5);
+  return (
+    <section
+      id="skills"
+      ref={ref}
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+    >
+      <SectionHeading text="My Skills" />
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {SKILLS_DATA?.map((skill, index) => (
+          <motion.li
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+            key={index}
+            className="rounded-xl border border-black/10 bg-white px-5 py-3"
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
